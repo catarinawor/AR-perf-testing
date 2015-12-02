50a507e790f610a120fd85226f632f93f1cd467a
kellijohnson <kellifayejohnson@gmail.com>
2015-12-02 10:03:07 -0800

    major changes to power analysis script

--- AR_power.R
+++ AR_power.R
@@ -1,118 +1,126 @@
-\documentclass{article}
-\usepackage[T1]{fontenc}
+#' Perform a power analysis for AR-perf-testing to determine how many samples
+#' are needed per year for age and length compositions to internally estimate
+#' autocorrelation inside Stock Synthesis.
 
-\begin{document}
-Scenarios (\Sexpr{ss3sim::id_scenarios(getwd())}) with
-\Sexpr{max(dir(ss3sim::id_scenarios(getwd())[1], pattern = "[0-9]$"))} iterations,
-where there are age- and length-compositions per sampling year
-for both the fishery and the survey.
-
-<<setup, include=FALSE>>=
-options(xtable.comment = FALSE)
-@
-
-<<echo = FALSE>>=
-library(ss3sim)
-library(xtable)
-scalars <- read.csv("ss3sim_scalar.csv", header = TRUE)
-ts <- read.csv("ss3sim_ts.csv", header = TRUE)
-scalars <- calculate_re(scalars, TRUE)
-
-ts$SSB_re <- with(ts, ((SpawnBio_om - SpawnBio_em) / SpawnBio_om))
-ts$R0_re <- with(ts, ((Recruit_0_om - Recruit_0_em) / Recruit_0_om))
-ts$F_re <- with(ts, ((F_om - F_em) / F_om))
-@
-
-<<echo = FALSE, fig.height = 8>>=
-numscen <- length(unique(ts$scenario))
-scenarios <- as.character(unique(ts$scenario))
-for (scen in 1:numscen) {
-par(mfrow = c(3, 1), mar = c(0, 5, 0, 1), oma = c(7, 1, 2, 2),
-  tck = -0.001, mgp = c(2.75, 0.15, 0), las = 1)
-for (par in c("SSB_re", "R0_re", "F_re")) {
- # Time series boxplot
-x <- aggregate(eval(parse(text = par)) ~ year,
-  data = ts[ts$scenario == scenarios[scen], ], mean)
-boxplot(eval(parse(text = par)) ~ year,
-    data = ts[ts$scenario == scenarios[scen], ], ylab = par,
-  ylim = c(-1, 1), xaxt = "n")
-getdatanum <- ss3sim:::get_caseval(scenarios[scen], case = "D")
-getlnthnum <- ss3sim:::get_caseval(scenarios[scen], case = "L")
-abline(h = 0, lty = 2, col = "red")
-  if (par == "SSB_re") {
-    legend("topright", legend = "legend reveals years with data", bty = "n")
-    legend("bottomright",
-      legend = c("Lognormal", "Multinomial", "Dirichlet"), pch = c(3, 19, 24),
-      bty = "n")
-    x.mast <- ss3sim:::get_args(file.path("..", "cases",
-      paste0("index", getdatanum,"-cod.txt")))
-    l.mast <- ss3sim:::get_args(file.path("..", "cases",
-      paste0("lcomp", getlnthnum, "-cod.txt")))
-
-    height <- 0.8
-    y <- par("yaxp")[2]
-    x <- x.mast$years[[1]]
-    points(x, rep(y * height, length(x)), pch = 3)
-    text(-4, par("yaxp")[2] * height,
-      paste0("survey (CV=", x.mast$sds_obs[[1]], ")"), pos = 4)
-
-    for (comps in seq_along(l.mast$fleets)){
-    height <- height - 0.15
-    type <- ifelse(l.mast$cpar[[comps]] == 1, "fish", "surv")
-      x <- l.mast$years[[comps]]
-      points(x, rep(y * height, length(x)),
-        pch = ifelse(l.mast$cpar[comps] == 1, 19, 24))
-      text(-4, par("yaxp")[2] * height,
-        paste0(type, " comp (n=", l.mast$Nsamp[[comps]][1], ")"), pos = 4)
-    }
-  }
+#' Ensure that cod was the last species simulated to ensure that
+#' the recruitment deviations will be specific for cod
+if (spp != "cod") {
+  stop(paste("The last species ran was", spp, "and not cod.\n",
+    "Please generate recruitment deviations specific for cod\n",
+    "before running the rest of AR_steepness.R -KFJ"))
 }
-  par(new = TRUE)
-  plot(1:100, rep(1, 100), ylab = "", xlab = "", yaxt = "n", type = "n")
-  axis(1)
-  mtext(side = 1, outer = TRUE, "years", line = 2)
-  legend("bottomright", legend = "*scales (-1:1) are symmetrical.",
-    bty = "n")
-  mtext(side = 1, outer = TRUE, line = 4,
-    paste0("Figure", scen, ". Relative error for ", scenarios[scen],
-      ifelse(length(dir(scenarios[scen], pattern = "bias")) == 0, " without", " with"),
-      " bias adjustment."))
-}
-@
 
-<<echo = FALSE, results = 'asis'>>=
-for (scen in 1:numscen){
-temp <- list()
-counter <- 1
-store <- vector()
-mynames <- grep("_re", names(scalars), value = FALSE)
-for(ind in mynames) {
-  if (!all(is.numeric(scalars[scalars$scenario == scenarios[scen], ind]))) next
-  if (all(is.na(scalars[scalars$scenario == scenarios[scen], ind]))) next
-  if (all(scalars[scalars$scenario == scenarios[scen], ind] == 0)) next
-  temp[[counter]] <- aggregate(as.matrix(scalars[scalars$scenario == scenarios[scen], ind]) ~ species,
-    data = scalars[scalars$scenario == scenarios[scen], ],
-    mean, na.rm = TRUE)
-  if (grepl("LnQ", colnames(scalars)[ind])) {
-    temp[[counter]] <- aggregate((
-      exp(scalars[scalars$scenario == scenarios[scen], "LnQ_base_2_Survey_om"]) -
-      exp(scalars[scalars$scenario == scenarios[scen], "LnQ_base_2_Survey_em"])) /
-      exp(scalars[scalars$scenario == scenarios[scen], "LnQ_base_2_Survey_om"]) ~
-      species,
-      data = scalars[scalars$scenario == scenarios[scen], ], mean,
-      na.rm = TRUE)
-  }
-  counter <- counter + 1
-  store <- append(store, ind, length(store) + 1)
-}
-temp <- do.call("rbind", sapply(temp, "[", 2))
-rownames(temp) <- colnames(scalars)[store]
-print(xtable(temp, digits = 5,
-  caption = paste0("Relative error in estimated and derived parameters, for",
-    scenarios[scen], ". Note q is exp(ln(q)).")), include.rownames = TRUE,
-  include.colnames = FALSE,
-  )
-}
-@
 
-\end{document}
+#' Setup
+nyears.min <- 10
+nyears.power <- seq(start + burnin, nyears - my.forecasts - nyears.min, by = 100)
+data.power <- c(200, 500)
+scenarios.power <- NULL
+basenumber <- 900000
+
+
+#' Write new case files
+setwd(case_folder)
+for (i in seq_along(nyears.power)) {
+  scenarios.power <- c(scenarios.power,
+    expand_scenarios(species = my.spp, cases = list(
+    D = 90 + i,
+    E = basenumber/1000 + my.forecasts,
+    A = basenumber + 10000 * i + data.power,
+    # L = c(0, basenumber + 10000 * i + data.power),
+    L = c(basenumber + 10000 * i + data.power),
+    F = 90 + i)))
+#' Fishing history
+  writeF(fvals =
+    c(rep(0, nyears.power[i] - 1),  # burnin with no fishing
+      rep(fmsy, nyears + start - nyears.power[i] - my.forecasts), # fishing at Fmsy
+      rep(0, my.forecasts)), # forecast period with no fishing
+    species = spp, case = 90 + i,
+    comment = "#Power analysis\n#No fishing during forecast\n",
+    start = start, end = nyears)
+#' Composition data
+  for (ii in seq_along(data.power)) {
+  for (iii in c("agecomp", "lcomp")){
+    case_comp(fleets = 2,
+      Nsamp = list(rep(data.power[ii], length(nyears.power[i]:(nyears - my.forecasts)))),
+      years = list(nyears.power[i]:(nyears - my.forecasts)),
+      cpar = 1,
+      type = iii,
+      case = basenumber + 10000 * i + data.power[ii], spp = my.spp)
+  }}
+#' Index data
+  case_index(fleets = 2,
+    years = list(nyears.power[i]:(nyears - my.forecasts)),
+    sd = list(my.dats[3]), case = 90 + i, spp = my.spp)
+}
+#' Fix fishery selectivity at the truth
+writeLines(c("natM_type; 1Parm", "natM_n_breakpoints; NULL",
+  "natM_lorenzen; NULL", "natM_val; c(NA, -1)",
+  "par_name; c(\"SizeSel_1P_1_Fishery\", \"SizeSel_1P_3_Fishery\")",
+  paste("par_int; c(50.8, 5.1)"),
+  "par_phase; rep(-99, 2)", "forecast_num; 20"), paste0("E920-", my.spp, ".txt"))
+#' Return to the main folder
+setwd("..")
+
+
+#' Run the simulation
+  # for (ar in 1:length(AR)) {
+  for (ar in 5) {
+    run_ss3sim(
+      iterations = 1:N,
+      scenarios = scenarios.power,
+      user_recdevs = EpsList[[ar]],
+      bias_adjust = FALSE,
+      om_dir = file.path(spp, "om"), em_dir = file.path(spp, "em"),
+      hess_always = TRUE,
+      parallel = doparallel, parallel_iterations = doparallel,
+      case_files = my.cases, case_folder = case_folder,
+      user_recdevs_warn = verbose
+    )
+
+    sppname <- paste0(substr(spp, 1, 1), letters[ar], "y")
+    for (scen in grep(spp, scenarios.power, value = TRUE)) {
+      # Copy to a new species name for the level of autocorrelation
+      newscenario <- gsub(spp, sppname, scen)
+      rename_folder(scen, newscenario, parallel = doparallel, numcores = numcores)
+      # Copy the no bias to a new name before estimating the ramp
+      system(paste("xcopy", newscenario, gsub("y$", "n", newscenario), "/E /S /H /I"),
+         show.output.on.console = verbose)
+      # Perform the bias ramp adjustment
+      foreach(it = 1:N) %dopar% {
+        fitbias(scenario = newscenario, iteration = it,
+        dir = dir.main, numberofcolumns = ncols, verbose = verbose)
+      }
+
+    # # z == Set AR to zero; t == Set AR to true AR; x == externally estimate AR
+    # for (type in "x") {
+    #   # Copy folder with new species name given the type
+    #   system(paste("xcopy", newscenario, gsub(spp, paste0(sppname, type), scen),
+    #     "/E /S /H /I"), show.output.on.console = verbose)
+    #   # Use parallel processing to loop through each iteration
+    #   foreach(it = 1:N) %dopar% {
+    #     em(it = it, type = type, truear = NULL, dir = dir.main,
+    #     sppold = spp, sppnew = sppname, scenario = scen, verbose = verbose)
+    #   } # End loop over each iteration
+    # } # End loop over each type of EM
+   } # End loop over each scenario
+  } # End loop over AR level
+
+get_results_all(overwrite_files = TRUE, parallel = doparallel)
+sc <- read.csv("ss3sim_scalar.csv")
+sc$bias <- ifelse(sc$max_bias_adj_em == -1, "no", "yes")
+sc$A <- factor(as.character(sc$A), levels = levels(sc$A), labels = gsub("91", "-", levels(sc$A)))
+sc$L <- factor(as.character(sc$L), levels = levels(sc$L), labels = gsub("91", "-", levels(sc$L)))
+
+g <- ggplot(subset(sc, E == "E20")) +
+theme_bw() +
+  theme(plot.background = element_blank(),
+        strip.background = element_blank(),
+        panel.border = element_rect(colour = "black"),
+        legend.key = element_rect(colour = "white"),
+        legend.title = element_text(size = 0, colour = "white")
+  ) +
+geom_boxplot(aes(x = bias, y = SR_autocorr_em)) +
+facet_grid(A ~ L, scales = "fixed") +
+geom_hline(yintercept = AR[5], col = "red")
+ggsave(file.path(fig_folder, "power_rho.png"), g, height = 8, width = 8)
