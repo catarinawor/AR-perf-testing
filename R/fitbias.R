384be25fef552b5d5db9938cd8ae336fdb39ee8a
kellijohnson <kellifayejohnson@gmail.com>
2015-12-17 08:29:51 -0800

    Add bias script

---
+++ R/fitbias.R
@@ -0,0 +1,22 @@
+fitbias <- function(scenario, iteration, dir, numberofcolumns = 300, verbose) {
+
+  old_wd <- getwd()
+  on.exit(setwd(old_wd))
+
+  empath <- file.path(dir, scenario, iteration, "em")
+  failed <- paste("Fail:", empath)
+  if (!file.exists(file.path(empath, "covar.sso"))) return(failed)
+  if (!file.exists(file.path(empath, "Report.sso"))) return(failed)
+
+  report <- r4ss::SS_output(empath, ncols = numberofcolumns, verbose = verbose,
+    forecast = FALSE, printstats = verbose)
+  r4ss::SS_fitbiasramp(report, verbose = verbose, print = verbose, plot = verbose,
+    oldctl = file.path(empath, "control.ss_new"),
+    newctl = file.path(empath, "em.ctl"))
+
+  keepgoodfiles(dir = empath)
+  bin <- paste(ss3sim::get_bin(), "exe", sep = ".")
+  file.copy(bin, file.path(empath, basename(bin)))
+  setwd(empath)
+  system(basename(bin), invisible = TRUE, show.output.on.console = verbose)
+}
